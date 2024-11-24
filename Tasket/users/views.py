4dd1b2968dd951c3e0723c180a03ed01b56b5dc5
matotski <matotski@mail.ru>
2024-11-24 20:43:35 +0300

    fix views for comments and tasks

--- Tasket/users/views.py
+++ Tasket/users/views.py
@@ -69,13 +69,33 @@ class RoleViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend, drf_filters.OrderingFilter]
-    filterset_class = TaskFilter
-    ordering_fields = ['status', 'priority', 'assigned_to', 'created_at', 'updated_at', 'title']
-    ordering = ['created_at']
+
+    def get_queryset(self):
+        project_id = self.kwargs.get('project_pk')
+        return self.queryset.filter(project_id=project_id)
+
+    def create(self, request, project_pk=None):
+        if not Project.objects.filter(pk=project_pk).exists():
+            return Response({"error": "Project does not exist."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        task = serializer.save(project_id=project_pk)
+        return Response(self.get_serializer(task).data, status=status.HTTP_201_CREATED)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        task_id = self.kwargs.get('task_pk')
+        return self.queryset.filter(task_id=task_id)
+
+    def create(self, request, project_pk=None,task_pk=None):
+        if not Task.objects.filter(pk=task_pk, project_id=project_pk).exists():
+            return Response({"error": "Task does not exist in this project."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        comment = serializer.save(task_id=task_pk)
+        return Response(self.get_serializer(comment).data, status=status.HTTP_201_CREATED)
 
